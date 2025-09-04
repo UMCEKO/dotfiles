568f2696acab874336c4b853a5a9c8bf7b2237b7
UMCEKO <umutcevdetkocak@gmail.com>
2025-09-04 23:15:04 +0300

    fix: script import

--- modules/home/scripts/default.nix
+++ modules/home/scripts/default.nix
@@ -1,11 +1,14 @@
-{ ... }: {
-  imports = [
-    ./emopicker.nix
-    ./nvidia-offload.nix
-    ./rofi-launcher.nix
-    ./screenshootin.nix
-    ./smart-switch-workspace.nix
-    ./task-waybar.nix
-    ./wallsetter.nix
+{ pkgs, username, ... }: {
+  home.packages = [
+    (import ./emopicker.nix { inherit pkgs; })
+    (import ./nvidia-offload.nix { inherit pkgs; })
+    (import ./rofi-launcher.nix { inherit pkgs; })
+    (import ./screenshootin.nix { inherit pkgs; })
+    (import ./smart-switch-workspace.nix { inherit pkgs; })
+    (import ./task-waybar.nix { inherit pkgs; })
+    (import ./wallsetter.nix {
+      inherit pkgs;
+      inherit username;
+    })
   ];
 }
