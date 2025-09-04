9e3e8cc4ae489d14455ab371aa0f96bd3ead57ab
UMCEKO <umutcevdetkocak@gmail.com>
2025-09-04 23:07:59 +0300

    fix: add default for scripts

---
+++ modules/home/scripts/default.nix
@@ -0,0 +1,11 @@
+{ ... }: {
+  imports = [
+    ./emopicker.nix
+    ./nvidia-offload.nix
+    ./rofi-launcher.nix
+    ./screenshootin.nix
+    ./smart-switch-workspace.nix
+    ./task-waybar.nix
+    ./wallsetter.nix
+  ];
+}
