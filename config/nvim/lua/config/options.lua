f4c627c0d0a0ef3a4ddf771244c7c60705f6bba4
UMCEKO <umutcevdetkocak@gmail.com>
2025-11-26 12:57:38 +0300

    feat: add osc52 support for ssh sessions

--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -7,3 +7,28 @@ vim.api.nvim_create_autocmd("BufReadPre", {
     vim.b.autoformat = false
   end,
 })
+
+-- Detect SSH
+local function is_ssh()
+  return os.getenv("SSH_TTY") ~= nil
+end
+
+if is_ssh() then
+  -- Use OSC52 for clipboard in SSH
+  local osc52 = require("vim.ui.clipboard.osc52")
+
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      ["+"] = osc52.paste("+"),
+      ["*"] = osc52.paste("*"),
+    },
+  }
+
+  -- Also set clipboard option
+  vim.opt.clipboard:append("unnamedplus")
+end
